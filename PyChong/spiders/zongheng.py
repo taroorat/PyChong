ee10f218605bd0fdd14a4ad92930a45448dd4b71
taroorat <2548316823@qq.com>
2019-09-05 22:37:30 +0800

    update

--- PyChong/spiders/zongheng.py
+++ PyChong/spiders/zongheng.py
@@ -5,7 +5,8 @@ import scrapy
 class ZonghengSpider(scrapy.Spider):
     name = 'zongheng'
     allowed_domains = ['zongheng.com']
-    start_urls = ['http://zongheng.com/']
+    start_urls = ['http://book.zongheng.com/chapter/672340/36898237.html']
 
     def parse(self, response):
-        pass
+        print(response.css('p').extract_first())
+
