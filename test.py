33e0eb772dfa370948c9ffb35eb3d3244d31669b
chenrq <chenrq@homewell.com.cn>
2019-09-12 17:19:56 +0800

    生成器测试

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-a=(i*2 for i in range(10))
-print(next(a))
-print(next(a))
-print(next(a))
+# a=(i*2 for i in range(10))
+# print(next(a))
+# print(next(a))
+# print(next(a))
+# print(a.__next__())
+def Fun(n):
+    a=0
+    b=1
+    while(n> 0):
+        n=n-1
+        a, b=b,b+a
+        yield a
+        # print(a)
+a=Fun(100)
+print(a.__next__())
+print(a.__next__())
+print(a.__next__())
+print(a.__next__())
 print(a.__next__())
