3aa8e3d19f87159615af9143438fa55133167fb7
chenrq <chenrq@homewell.com.cn>
2019-09-12 10:34:28 +0800

    读取全本小说

--- PyChong/spiders/zongheng.py
+++ PyChong/spiders/zongheng.py
@@ -10,8 +10,14 @@ class ZonghengSpider(scrapy.Spider):
     def parse(self, response):
         # print(response.css('p').re('<p>(.*)</p>'))
         # print(response.text)
+        nextUrl=""
         for content in response.css('p').re('<p>(.*)</p>'):
+            with open ("../files/test.txt","a") as f:
+                f.write("\n")
+                f.write(content)
             print(content)
         for nextUrl in response.css('div.chap_btnbox').re('<a href="(.*)" class=.*\}\'>.*</a>'):
             print(nextUrl)
+
+        yield scrapy.Request(url=nextUrl, callback=self.parse)
 
