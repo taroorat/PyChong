d9e9f437210f6fc55ad1082e02616248af8e57c7
taroorat <2548316823@qq.com>
2019-09-15 01:13:34 +0800

    格式化json数据

--- PyChong/spiders/feixiaohao.py
+++ PyChong/spiders/feixiaohao.py
@@ -6,14 +6,16 @@ import json
 class FeixiaohaoSpider(scrapy.Spider):
     name = 'feixiaohao'
     allowed_domains = ['feixiaohao.com','bqiapp.com']
-    start_urls = ['https://dncapi.bqiapp.com/api/v2/ranking/coinvol?per_page=100&page=1&sort=vol_m&webp=1']
+    start_urls = ['https://dncapi.bqiapp.com/api/v2/ranking/coinvol?per_page=10&page=1&sort=vol_m&webp=1']
 
     def parse(self, response):
         responseData = []
         for data in response.css('p').re('<p>(.*)</p>'):
             responseData=data
-        print(json.loads(responseData))
+            responseData = json.loads(responseData)
+            print(json.dumps(responseData,indent=4))
 
-        # soup = BeautifulSoup(responseTest, 'lxml')
-        # print(soup.prettify())
+
+
+
 
