f56e9756b4ccc69e6a07f867fee63a43802a3c8a
taroorat <2548316823@qq.com>
2019-09-15 01:05:29 +0800

    获取成交量排名数据

--- PyChong/spiders/feixiaohao.py
+++ PyChong/spiders/feixiaohao.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from bs4 import BeautifulSoup
+import json
 
 class FeixiaohaoSpider(scrapy.Spider):
     name = 'feixiaohao'
-    allowed_domains = ['feixiaohao.com']
-    start_urls = ['http://feixiaohao.com/']
+    allowed_domains = ['feixiaohao.com','bqiapp.com']
+    start_urls = ['https://dncapi.bqiapp.com/api/v2/ranking/coinvol?per_page=100&page=1&sort=vol_m&webp=1']
 
     def parse(self, response):
-        responseTest=response.text
-        soup = BeautifulSoup(responseTest, 'lxml')
-        print(soup.prettify())
+        responseData = []
+        for data in response.css('p').re('<p>(.*)</p>'):
+            responseData=data
+        print(json.loads(responseData))
+
+        # soup = BeautifulSoup(responseTest, 'lxml')
+        # print(soup.prettify())
+
