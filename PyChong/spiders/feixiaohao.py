ea6ae3d0b347b79136d986e43875bfd4bcd1c673
taroorat <2548316823@qq.com>
2019-09-15 16:17:25 +0800

    update

--- PyChong/spiders/feixiaohao.py
+++ PyChong/spiders/feixiaohao.py
@@ -6,7 +6,7 @@ import json
 class FeixiaohaoSpider(scrapy.Spider):
     name = 'feixiaohao'
     allowed_domains = ['feixiaohao.com','bqiapp.com']
-    start_urls = ['https://dncapi.bqiapp.com/api/coin/web-coinrank?page=1&type=-1&pagesize=5&webp=1']
+    start_urls = ['https://dncapi.bqiapp.com/api/coin/web-coinrank?page=1&type=-1&pagesize=100&webp=1']
 
     def parse(self, response):
         responseData = []
@@ -14,9 +14,9 @@ class FeixiaohaoSpider(scrapy.Spider):
             responseData=data
             responseData = json.loads(responseData)
             # print(json.dumps(responseData,indent=4))
-            print(type(responseData['data']))
+            # print(type(responseData['data']))
             for data in responseData['data']:
-                print(data)
+                print(data['name'])
 
 
 
