50025ec08a97759c8883c76f5a22bd3894a72d4e
taroorat <2548316823@qq.com>
2019-09-15 16:13:35 +0800

    获取市值排名

--- PyChong/spiders/feixiaohao.py
+++ PyChong/spiders/feixiaohao.py
@@ -6,7 +6,7 @@ import json
 class FeixiaohaoSpider(scrapy.Spider):
     name = 'feixiaohao'
     allowed_domains = ['feixiaohao.com','bqiapp.com']
-    start_urls = ['https://dncapi.bqiapp.com/api/v2/ranking/coinvol?per_page=10&page=1&sort=vol_m&webp=1']
+    start_urls = ['https://dncapi.bqiapp.com/api/coin/web-coinrank?page=1&type=-1&pagesize=5&webp=1']
 
     def parse(self, response):
         responseData = []
@@ -14,8 +14,8 @@ class FeixiaohaoSpider(scrapy.Spider):
             responseData=data
             responseData = json.loads(responseData)
             # print(json.dumps(responseData,indent=4))
-            print(type(responseData['data']['list']))
-            for data in responseData['data']['list']:
+            print(type(responseData['data']))
+            for data in responseData['data']:
                 print(data)
 
 
