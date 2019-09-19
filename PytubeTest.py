7e095ec8ec2348b74ffc0d26f3149f724e40b848
taroorat <2548316823@qq.com>
2019-09-19 23:06:15 +0800

    update

--- PytubeTest.py
+++ PytubeTest.py
@@ -8,7 +8,7 @@ from pytube import YouTube
 # urllib.request.install_opener(opener)
 # #输入要下载的视频的路径
 # print(1)
-####### python 选3.5.3可用，当前3.7 报证书错误
+####### python 选3.5.3可用，当前3.7 报证书错误（mac下）
 url='https://youtube.com/watch?v=9bZkp7q19f0'
 yt = YouTube(url)
 print(yt.streams.all())
