f12ef5ff9aacfce595f7a9e6347fd5c6136c4220
taroorat <2548316823@qq.com>
2019-09-19 23:12:24 +0800

    pytube ---- python 选3.5.3可用，当前3.7 报证书错误（mac下）widows3.7下可正常使用

--- PytubeTest.py
+++ PytubeTest.py
@@ -1,14 +1,16 @@
 import urllib.request
 from pytube import YouTube
 
+### 设置代理
 # proxy_addr = 'http://127.0.0.1:1087'
 # proxy = urllib.request.ProxyHandler({'http':proxy_addr})
 #
 # opener = urllib.request.build_opener(proxy,urllib.request.HTTPHandler)
 # urllib.request.install_opener(opener)
-# #输入要下载的视频的路径
-# print(1)
+
+
 ####### python 选3.5.3可用，当前3.7 报证书错误（mac下）
+####### widows3.7下可正常使用
 url='https://youtube.com/watch?v=9bZkp7q19f0'
 yt = YouTube(url)
 print(yt.streams.all())
