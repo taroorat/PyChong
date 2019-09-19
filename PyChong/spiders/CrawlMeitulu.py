bea5e44a8a5c11718de215e90df4833fea37aa6a
chenrq <chenrq@homewell.com.cn>
2019-09-19 17:07:12 +0800

    CrawlMeitulu暂时title为test

--- PyChong/spiders/CrawlMeitulu.py
+++ PyChong/spiders/CrawlMeitulu.py
@@ -7,21 +7,27 @@ from PyChong.items import MeituluItem
 class CrawlmeituluSpider(CrawlSpider):
     name = 'CrawlMeitulu'
     allowed_domains = ['meitulu.com','ttsqgs.com']
+    # start_urls = ['http://meitulu.com/t/nvshen/']
     start_urls = ['http://meitulu.com/']
 
     rules = (
         Rule(LinkExtractor(allow=r'item/\d+?'), callback='parse_item', follow=False),
+        # Rule(LinkExtractor(allow=r't/nvshen/\d+?'), follow=True),
     )
-    #CrawlMeitulu目前代码思路错误
+
     def parse_item(self, response):
         image_list = response.xpath('//center/img/@src')
+        print(response)
+        title="test"
         # print(image_list)
         img_urls = []
         for image in image_list:
-            print(image)
+            # print(image)
             image_url=image.re('(.*)')[0]
-            print(image_url)
+            # print(image_url)
             img_urls.append(image_url)
-            item = MeituluItem(image_urls=img_urls)
-            request = scrapy.Request(url=img_urls, callback=self.parse_item)
-            yield request
+        url=response.xpath('//div[@class="boxs"]//a/@href').extract_first()
+        print(url)
+        yield scrapy.Request(url=url, callback=self.parse_item,meta={'title' : title})
+        item = MeituluItem(image_urls=img_urls,title=response.meta['title'])
+        yield item
